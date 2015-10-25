bcdb86d84588cd81c954bf8ed9494cb26826912a
avaika <social@irush.su>
2015-10-25 23:04:05 +0300

    handle index page properly

--- project/urls.py
+++ project/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns(
     (r'^favicon\.ico$', RedirectView.as_view(url='/static/base/img/favicon.ico', permanent=True)),
     (r'^robots\.txt$', RedirectView.as_view(url='/static/robots.txt', permanent=True)),
     url('', include('social.apps.django_app.urls', namespace='social')),
+    # to be replaced by redirect
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^comments/', include('fluent_comments.urls')),
     url(r'^adminfiles/', include('project.adminfiles.urls')),
