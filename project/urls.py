1933e18e38bddcd8bb039ef011d2d4188f331e10
avaika <social@irush.su>
2016-09-25 02:08:29 +0300

    remove deprecated url.patterns

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import RedirectView, TemplateView
 from django.contrib import admin
 from django.conf import settings
@@ -6,11 +6,10 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    (r'^grappelli/', include('grappelli.urls')),
-    (r'^favicon\.ico$', RedirectView.as_view(url='/static/base/img/favicon.ico', permanent=True)),
-    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/base/img/favicon.ico', permanent=True)),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     # to be replaced by redirect
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
@@ -18,16 +17,18 @@ urlpatterns = patterns(
     url(r'^comments/', include('fluent_comments.urls')),
     url(r'^tags_input/', include('tags_input.urls', namespace='tags_input')),
     url(r'^sudo/', include(admin.site.urls)),
+]
+
+urlpatterns += i18n_patterns(
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/passwordsent/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
+    url(r'^i/', include('project.travel.urls')),
+    url(r'^blog/', include('project.blog.urls')),
+    url(r'^books/', include('project.books.urls')),
 )
 
-urlpatterns += i18n_patterns('',
-                             (r'^accounts/', include('allauth.urls')),
-                             url(r'^accounts/passwordsent/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-                             url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
-                             url(r'^i/', include('project.travel.urls')),
-                             url(r'^blog/', include('project.blog.urls')),
-                             url(r'^books/', include('project.books.urls')),
-                             )
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'project.travel.views.error404'
