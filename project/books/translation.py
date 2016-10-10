90b6475c5519c5c9180ba42a17cfc335d3485503
avaika <social@irush.su>
2016-10-11 01:00:34 +0300

    update books translation

---
+++ project/books/translation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from modeltranslation.translator import translator, TranslationOptions
+from .models import Author, Genre, Book
+
+
+class AuthorTranslationOptions(TranslationOptions):
+        fields = ('value', 'wiki_url')
+translator.register(Author, AuthorTranslationOptions)
+
+
+class GenreTranslationOptions(TranslationOptions):
+        fields = ('value', )
+translator.register(Genre, GenreTranslationOptions)
+
+
+class BookTranslationOptions(TranslationOptions):
+        fields = ('title', 'wiki_url', 'about')
+translator.register(Book, BookTranslationOptions)
