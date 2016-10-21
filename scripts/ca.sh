d3f07036d159fad206d8e25e6fb8dc28d1696ac7
avaika <social@irush.su>
2016-10-22 00:39:40 +0300

    changes in helper script

--- scripts/ca.sh
+++ scripts/ca.sh
@@ -5,18 +5,26 @@ error() {
     exit 1
 }
 
+[ "$1" == "-l" ] && work_local="true" && shift
+database="${1:-blog}"
+db_file="avaika.sql"
+
 download() {
-  ssh avaika 'set -x ; mysqldump avaika > avaika.sql && gzip avaika.sql' || error "Failed to ssh"
-  rsync -e ssh  --progress -Pa --inplace --no-whole-file avaika:avaika.sql.gz .  || error "Failed to copy"
-  ssh avaika 'rm avaika.sql*' || error "Failed to ssh"
-  gunzip avaika.sql.gz || error "Failed to unarchive"
+  ssh avaika "set -x ; mysqldump avaika > $db_file && gzip $db_file" || error "Failed to ssh"
+  rsync -e ssh  --progress -Pa --inplace --no-whole-file avaika:${db_file}.gz .  || error "Failed to copy"
+  ssh avaika "rm ${db_file}*" || error "Failed to ssh"
+  gunzip "${db_file}.gz" || error "Failed to unarchive"
 }
 
 import() {
-  mysql blog < avaika.sql || error "Failed to import"
+  mysql "$database" -N -e 'show tables;' | while read table; do 
+      mysql "$database" -e "SET FOREIGN_KEY_CHECKS = 0; drop table $table;"; 
+  done 
+  mysql "$database" < "$db_file" || error "Failed to import"
 }
 
-[ "$1" == "-l" ] || download
+[ -n "$work_local" ] || download
+[ -f "$db_file" ] || error "No dump found // $db_file"
 import
-[ "$1" == "-l" ] || rm -i avaika.sql
+[ -n "$work_local" ] || rm -i "$db_file"
 echo -n
