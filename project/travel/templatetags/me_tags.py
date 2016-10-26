704cde611838eebf01bf643da8b523bad4fc7e89
avaika <social@irush.su>
2016-10-27 01:55:13 +0300

    fix balls

--- project/travel/templatetags/me_tags.py
+++ project/travel/templatetags/me_tags.py
@@ -50,7 +50,7 @@ def get_flag_current(item, first, last):
 
 @register.assignment_tag()
 def flags(page_posts):
-    posts = Post.objects.filter(draft=False, country__flag__isnull=False).order_by('-created').values('created', 'country__value', 'country__flag', 'country_ball')
+    posts = Post.objects.filter(draft=False, country__flag__isnull=False).order_by('-created').values('created', 'country__value', 'country__flag')
     first = page_posts[0]
     last = page_posts[len(page_posts) - 1]
     flags = []
