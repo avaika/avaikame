e4aeb978da8815c9ba84d34e5e977c7ec6b7e36d
avaika <social@irush.su>
2018-01-20 13:04:14 +0300

    queryset wa

--- project/books/views.py
+++ project/books/views.py
@@ -3,7 +3,10 @@ from models import Book
 
 
 class BookListView(ListView):
-    # queryset = Book.objects.all
+    # somehow with postgres django returns duplicate entries without
+    # defined queryset. need to investigate why it happens
+    # works fine with mysql though...
+    queryset = Book.objects.all()
     model = Book
     paginate_by = 100
     context_object_name = "books"
