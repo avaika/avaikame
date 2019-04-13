e2e8364ebeb7da70aa6440297f68df30bab004dd
avaika <github@avaika.me>
2019-04-14 00:23:07 +0300

    fix qs

--- project/books/views.py
+++ project/books/views.py
@@ -3,13 +3,13 @@ from models import Book
 
 
 class BookListView(ListView):
-    # somehow with postgres django returns duplicate entries without
-    # defined queryset. need to investigate why it happens
-    # works fine with mysql though...
-    Book.objects.filter(active=True).order_by('-created')
     model = Book
     paginate_by = 100
     context_object_name = "books"
     template_name = 'books/list.html'
+
+    def get_queryset(self):
+        qs = super(BookListView, self).get_queryset()
+        return qs.filter(active=True).order_by('-created')
 
 book_main = BookListView.as_view()
