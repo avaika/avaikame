a8c717cc06770a44d6835705bd9cc9ebe6098e58
avaika <github@avaika.me>
2020-10-11 20:13:31 +0300

    Missing list view

--- project/lists/urls.py
+++ project/lists/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.lists_main, name="lists_all"),
+    url(r'(?P<list_name>.*)/$', views.list_detail, name="list_detail"),
 ]
--- project/lists/views.py
+++ project/lists/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView
 from models import List, Entry
 
 
-class ListListView(ListView):
+class AllListsView(ListView):
     List.objects.all().order_by('-created')
     model = List
     paginate_by = 100
@@ -10,4 +10,17 @@ class ListListView(ListView):
     template_name = 'lists/list.html'
 
 
-lists_main = ListListView.as_view()
+lists_all = AllListsView.as_view()
+
+
+class ListDetailView(ListView):
+    model = Entry
+    paginate_by = 100
+    context_object_name = "items"
+    template_name = 'lists/item.html'
+
+    def get_queryset(self):
+        qs = super(ListDetailView, self).get_queryset()
+        return qs.filter(listItem=kwargs['list_name'], published=True)
+
+list_detail = ListDetailView.as_view()
