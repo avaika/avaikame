7defd47f724c6d13b7978a9142704af21a8949b2
Kangaroo <github@avaika.me>
2026-03-08 19:53:22 +0000

    Switch start to gunicorn

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ cd /app/avaika.me
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
+gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3
